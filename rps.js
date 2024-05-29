de5825440997c95965dcd0572f6ddcb7e0c6a508
Spicfy <68024083+Spicfy@users.noreply.github.com>
2024-05-29 03:05:04 -0400

    added gui  to game

--- rps.js
+++ rps.js
@@ -1,19 +1,14 @@
-console.log("Hello World");
+const choices = ['r', 'p', 's'];
+const playerDisplay=document.getElementById("playerDisplay");
+const computerDisplay=document.getElementById("computerDisplay");
+
+const resultDisplay = document.getElementById("resultDisplay");
+
+const winner = document.getElementById("winner");
+
 var human_score = 0;
 var computer_score = 0;
-function getComputerChoice() {
-    const choices = ['r', 'p', 's'];
-    var num = Math.floor(Math.random()*3);
-    if(num == 0){
-        return 'r';
-    }
-    else if(num ==1){
-        return 'p';
-    }
-    else{
-        return 's';
-    }
-}
+
 
 function getHumanChoice(){
     valid = false;
@@ -28,8 +23,20 @@ function getHumanChoice(){
     }
     return human_choice;
 }
-function playRound(humanChoice, computerChoice){
-     if(humanChoice == computerChoice){
+function playGame(humanChoice){
+    const choices = ['r', 'p', 's'];
+    var num = Math.floor(Math.random()*3);
+    var computerChoice;
+    if(num == 0){
+        computerChoice = 'r';
+    }
+    else if(num ==1){
+        computerChoice = 'p';
+    }
+    else{
+        computerChoice = 's';
+    }
+     if(humanChoice === computerChoice){
         console.log("It's a tie");
      }   
      else if((humanChoice == 'r' && computerChoice == 's') || (humanChoice == 's' && computerChoice =='p') || (humanChoice == 'p' && computerChoice == 'r')){
@@ -40,25 +47,28 @@ function playRound(humanChoice, computerChoice){
         console.log("Computer wins");
         computer_score++;
      }
+     if(computerChoice == 'r'){
+        computerChoice = 'Rock';
+     }
+     else if(computerChoice == 'p'){
+        computerChoice = 'Paper';
+     }
+     else{
+        computerChoice = 'Scissors';
+     }
+     playerDisplay.textContent = `Player: ${humanChoice}`;
+    computerDisplay.textContent = `Computer: ${computerChoice}`;
+     resultDisplay.textContent = `Human score: ${human_score} Computer score: ${computer_score}`;
+     if(human_score == 5){
+        winner.textContent = "Human wins the game";
+        human_score = 0;
+        computer_score = 0;
+    }
+
+     else if(computer_score == 5){
+        winner.textContent = "Computer wins the game";
+        human_score = 0;
+        computer_score = 0; 
+    }
 }
-function playGame(){
-    human_score = 0;
-    computer_score = 0;
-    for(var i = 0; i < 5; i++){
-        const humanChoice = getHumanChoice();
-        const computerChoice = getComputerChoice();
-        playRound(humanChoice, computerChoice);
-    }
-    console.log("Human score: " + human_score);
-    console.log("Computer score: " + computer_score);
-    if(human_score > computer_score){
-        console.log("Human wins the game");
-    }
-    else if(human_score < computer_score){
-        console.log("Computer wins the game");
-    }
-    else{
-        console.log("It's a tie");
-    }
-}
-playGame();
+
